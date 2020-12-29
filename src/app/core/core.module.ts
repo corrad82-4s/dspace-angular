c844e55b372e4ce1c2d93d746998e63552e57445
Corrado Lombardi <corrado.lombardi@4science.it>
2020-12-29 17:33:32 +0100

    WIP

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -428,7 +428,6 @@ export const models =
     VocabularyEntry,
     VocabularyEntryDetail,
     ConfigurationProperty,
-    UsageReport,
     SearchComponent,
     ResearcherProfile,
     ItemExportFormat,
