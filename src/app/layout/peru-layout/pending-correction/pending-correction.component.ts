973730a63ae3f4058d786a074b10f82c825e82e2
Alessandro Martelli <alessandro.martelli@4science.it>
2021-02-04 17:40:59 +0100

    [CSTPER-464] Deny list for pending information

--- src/app/layout/peru-layout/pending-correction/pending-correction.component.ts
+++ src/app/layout/peru-layout/pending-correction/pending-correction.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Item } from '../../../core/shared/item.model';
 import { take } from 'rxjs/operators';
-import { MetadataCorrection, PendingCorrectionService } from './pending-correction.service';
+import { CorrectionItem, MetadataCorrection, PendingCorrectionService } from './pending-correction.service';
 
 @Component({
   selector: 'ds-pending-correction',
@@ -12,19 +12,17 @@ export class PendingCorrectionComponent implements OnInit {
 
   @Input() item: Item;
 
-  correctionItem: Item;
-
-  corrections: MetadataCorrection[];
+  correctionItem: CorrectionItem;
 
   hide = true;
 
-  constructor(protected pendingCorrectionService: PendingCorrectionService) { }
+  constructor(protected pendingCorrectionService: PendingCorrectionService, protected _cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
 
-    this.pendingCorrectionService.getCorrectionItem(this.item).pipe(take(1)).subscribe((correctionItem) => {
+    this.pendingCorrectionService.getCorrectionItem(this.item).pipe(take(1)).subscribe((correctionItem: CorrectionItem) => {
       this.correctionItem = correctionItem;
-      this.corrections = this.pendingCorrectionService.getCorrectionMetadata(this.item, this.correctionItem);
+      this._cd.detectChanges();
     });
   }
 
