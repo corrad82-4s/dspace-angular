0ceff2ac20143a22f07325e6b85bf8c3ce674833
Luca Giamminonni <luca.giamminonni@4Science.it>
2021-03-19 18:22:17 +0100

    [CSTPER-510] Bind a CV profile to an existent HR profile

--- src/app/shared/context-menu/claim-item/claim-item-menu.component.ts
+++ src/app/shared/context-menu/claim-item/claim-item-menu.component.ts
@@ -9,12 +9,13 @@ import { DSpaceObjectType } from '../../../../app/core/shared/dspace-object-type
 import { DSpaceObject } from '../../../../app/core/shared/dspace-object.model';
 import { ContextMenuEntryComponent } from '../context-menu-entry.component';
 import { rendersContextMenuEntriesForType } from '../context-menu.decorator';
-import { getFirstSucceededRemoteData } from '../../../../app/core/shared/operators';
-import { mergeMap } from 'rxjs/operators';
+import { getFirstCompletedRemoteData, getFirstSucceededRemoteData } from '../../../../app/core/shared/operators';
+import { mergeMap, switchMap } from 'rxjs/operators';
 import { RemoteData } from '../../../../app/core/data/remote-data';
 import { ResearcherProfile } from '../../../../app/core/profile/model/researcher-profile.model';
 import { isNotUndefined } from '../../empty.util';
 import { Observable } from 'rxjs';
+import { AuthService } from '../../../../app/core/auth/auth.service';
 
 @Component({
   selector: 'ds-context-menu-claim-item',
@@ -36,26 +37,37 @@ export class ClaimItemMenuComponent extends ContextMenuEntryComponent {
     protected authorizationService: AuthorizationDataService,
     private researcherProfileService: ResearcherProfileService,
     private router: Router,
-    private notificationsService: NotificationsService
+    private notificationsService: NotificationsService,
+    private authService: AuthService
   ) {
     super(injectedContextMenuObject, injectedContextMenuObjectType);
   }
 
   claim() {
 
-    this.researcherProfileService.createFromExternalSource(this.injectedContextMenuObject.self)
-      .pipe(
-        getFirstSucceededRemoteData(),
-        mergeMap((rd: RemoteData<ResearcherProfile>) => {
-          return this.researcherProfileService.findRelatedItemId(rd.payload);
-        }))
-      .subscribe((id: string) => {
-        if (isNotUndefined(id)) {
-          this.router.navigateByUrl('/items/' + id);
+    this.authService.getAuthenticatedUserFromStore().pipe(
+      switchMap((user) => this.researcherProfileService.findById(user.id)),
+      switchMap((researcherProfile) => {
+        if(researcherProfile == null){
+          return this.researcherProfileService.createFromExternalSource(this.injectedContextMenuObject.self);
         } else {
-          this.notificationsService.error('researcherprofile.error.claim.title', 'researcherprofile.error.claim.body');
+          return this.researcherProfileService.claim(researcherProfile, this.injectedContextMenuObject.id);
         }
-        });
+      }),
+      getFirstSucceededRemoteData(),
+      mergeMap((rd: RemoteData<ResearcherProfile>) => {
+        console.log('1', rd);
+        return this.researcherProfileService.findRelatedItemId(rd.payload);
+      })
+    ).subscribe((id: string) => {
+      console.log('2', id);
+      if (isNotUndefined(id)) {
+        this.router.navigateByUrl('/items/' + id);
+      } else {
+        this.notificationsService.error('researcherprofile.error.claim.title', 'researcherprofile.error.claim.body');
+      }
+    });
+      
   }
 
   isClaimable(): Observable<boolean> {
