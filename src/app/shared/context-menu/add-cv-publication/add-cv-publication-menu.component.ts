7f90a895b7fe4791dfc0c0830d530674c684a0b2
Corrado Lombardi <corrado.lombardi@4science.it>
2021-04-30 00:54:43 +0200

    updated entity type check

--- src/app/shared/context-menu/add-cv-publication/add-cv-publication-menu.component.ts
+++ src/app/shared/context-menu/add-cv-publication/add-cv-publication-menu.component.ts
@@ -29,7 +29,7 @@ export class AddCvPublicationMenuComponent extends ContextMenuEntryComponent imp
   }
 
   ngOnInit(): void {
-    this.isCvProfile$.next(this.contextMenuObject.firstMetadataValue('relationship.type') === 'CvPerson');
+    this.isCvProfile$.next(this.contextMenuObject.firstMetadataValue('dspace.entity.type') === 'CvPerson');
   }
 
   isCvProfile(): Observable<boolean> {
